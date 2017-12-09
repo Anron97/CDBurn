621d4e5c90cb50fa20cc9a64dd9e2266f09fb28e
ptaxom <ptaxom@gmail.com>
2017-12-09 14:42:52 +0300

    final commit

--- src/CDBurn/CDBurn.java
+++ src/CDBurn/CDBurn.java
@@ -1,6 +1,8 @@
 package CDBurn;
 
 import CDBurn.Services.ExecuteCommandService;
+import javafx.application.Platform;
+import javafx.beans.property.StringProperty;
 
 import java.io.File;
 import java.util.LinkedList;
@@ -9,10 +11,19 @@ import java.util.List;
 public class CDBurn {
 
     private List<File> listFiles = new LinkedList<>();
+    private StringProperty progressValue;
+    private Controller controller;
+
+    public CDBurn(StringProperty progressValue, Controller controller) {
+        this.progressValue = progressValue;
+        this.controller = controller;
+
+    }
 
     public void addFile(File file) {
         listFiles.add(file);
     }
+
 
     public long getSizeAllFiles() {
        return (listFiles.stream().map(file -> file.length()))
@@ -20,32 +31,39 @@ public class CDBurn {
     }
 
     private void copyFiles() {
+        Platform.runLater(() -> progressValue.setValue("Copy files to temp directory DiskBuffer"));
         for (File file : listFiles) {
             ExecuteCommandService.Execute("cp -R " + file.getAbsolutePath() + " /home/ptaxom/DiskBuffer");
         }
     }
 
     private void createDirectory() {
+        Platform.runLater(() -> progressValue.setValue("Create temp directory DiskBuffer"));
         ExecuteCommandService.Execute("mkdir /home/ptaxom/DiskBuffer");
     }
 
     private void umountDisk() {
+        Platform.runLater(() -> progressValue.setValue("Umount disk"));
         ExecuteCommandService.Execute("umount /dev/sr0");
     }
 
     private void createISO() {
+        Platform.runLater(() -> progressValue.setValue("Create ISO Disk.iso"));
         ExecuteCommandService.Execute("mkisofs -v -J -o Disk.iso /home/ptaxom/DiskBuffer");
     }
 
     private void formatDisk() {
+        Platform.runLater(() -> progressValue.setValue("Format CD disk"));
         ExecuteCommandService.Execute("cdrecord -dev=/dev/sr0 -v -blank=fast");
     }
 
     private void writeFilesToDisk() {
+        Platform.runLater(() -> progressValue.setValue("Record Disk.iso to CD"));
         ExecuteCommandService.Execute("cdrecord -dev=/dev/sr0 -speed=16 -eject -v Disk.iso");
     }
 
     private void deleteISO() {
+        Platform.runLater(() -> progressValue.setValue("Delete temp files"));
         ExecuteCommandService.Execute("rm /home/ptaxom/Disk.iso");
     }
 
@@ -63,6 +81,9 @@ public class CDBurn {
             writeFilesToDisk();
             deleteISO();
             deleteFiles();
+            Platform.runLater(() -> progressValue.setValue("Complete!"));
+            controller.switchStateButtons();
+            listFiles.clear();
         }
     }
 
--- src/CDBurn/Controller.java
+++ src/CDBurn/Controller.java
@@ -1,10 +1,14 @@
 package CDBurn;
 
 import CDBurn.Services.Tools;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressBar;
 import javafx.stage.FileChooser;
@@ -21,9 +25,10 @@ public class Controller {
     @FXML
     private Button write;
     @FXML
-    private ProgressBar progressBar;
+    private Label  progress;
 
-    private CDBurn cdBurn = new CDBurn();
+    private StringProperty progressValue = new SimpleStringProperty();
+    private CDBurn cdBurn = new CDBurn(progressValue, this);
     private ObservableList<String> fileNames = FXCollections.observableArrayList();
     private FileChooser fileChooser = new FileChooser();
 
@@ -31,6 +36,7 @@ public class Controller {
     private void initialize() {
         listView.setItems(fileNames);
         fileChooser.setTitle("Choose file");
+        progress.textProperty().bind(progressValue);
     }
 
     public void chooseFile() {
@@ -40,8 +46,17 @@ public class Controller {
     }
 
     public void writeFiles() {
-            runAsynchronouslyWriteProcess();
+        fileChooserButton.setDisable(true);
+        write.setDisable(true);
+        runAsynchronouslyWriteProcess();
+    }
 
+    public void switchStateButtons() {
+        Platform.runLater(() -> {
+            fileChooserButton.setDisable(false);
+            write.setDisable(false);
+            fileNames.clear();
+        });
     }
 
     private void runAsynchronouslyWriteProcess() {
